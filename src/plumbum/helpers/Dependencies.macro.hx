0625d49e64a7f5ff2c622d5fddbfc70c89fc123a
Juraj Kirchheim <juraj.kirchheim@doclerholding.com>
2022-02-03 11:42:41 +0100

    Put optional fields last.

--- src/plumbum/helpers/Dependencies.macro.hx
+++ src/plumbum/helpers/Dependencies.macro.hx
@@ -13,14 +13,20 @@ class Dependencies {
       ret.meta.push({ name: ':structInit', pos: (macro null).pos });
       switch ctx.type {
         case TAnonymous(a):
-          for (f in a.get().fields)
-            ret.fields.push({
+          for (f in a.get().fields) {
+
+            function add(optional, f)
+              if (optional) ret.fields.push(f);
+              else ret.fields.unshift(f);
+
+            add(f.meta.has(':optional'), {
               access: [APublic],
               name: f.name,
               pos: f.pos,
               meta: f.meta.get(),
               kind: FProp('default', 'null', f.type.toComplex()),
             });
+          }
         default:
           ctx.pos.error('Type parameter should be structure');
       }
